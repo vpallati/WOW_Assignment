0e3bd7915a1d73fe7a6c8f8687d0d1de1274b613
Vishnu Vardhan Kumar Pallati <p.vishnuvardhankumar@gmail.com>
2017-07-07 11:40:45 -0400

    Implemented initial design of Display page and upload product page

---
+++ WOW_Assignment/Website/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+using Website.ViewModels;
+
+namespace Website.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+        private IProductBusiness _productBuisness;
+
+        public ProductController()
+        {
+            _productBuisness = new ProductBusiness();
+        }
+
+        [Route("DisplayProducts")]
+        public ActionResult DisplayProducts()
+        {
+            var model = _productBuisness.GetAllProducts();
+            return View(model.Select(a => ProductViewModel.Convert(a)));
+        }
+
+        [HttpGet]
+        [Route("UploadProduct")]
+        public ActionResult UploadNewProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UploadProduct")]
+        public bool UploadProduct(ProductViewModel model)
+        {
+            return (_productBuisness.AddProduct(ProductViewModel.Convert(model)));
+        }
+    }
+}
---
+++ WOW_Assignment/Website/ViewModels/ProductViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+
+        public static ProductModel Convert(ProductViewModel entity)
+        {
+            return new ProductModel()
+            {
+                ProductID = entity.ProductID,
+                Title = entity.Title,
+                Description = entity.Description,
+                ImageURL = entity.ImageURL
+            };
+        }
+
+        public static ProductViewModel Convert(ProductModel model)
+        {
+            return new ProductViewModel()
+            {
+                ProductID = model.ProductID,
+                Title = model.Title,
+                Description = model.Description,
+                ImageURL = model.ImageURL
+            };
+        }
+    }
+}
