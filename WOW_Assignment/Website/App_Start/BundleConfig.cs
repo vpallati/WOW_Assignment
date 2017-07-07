5d8090debb483e030fdf2bd518a751dec2ecdd43
Vishnu Vardhan Kumar Pallati <p.vishnuvardhankumar@gmail.com>
2017-07-07 13:12:54 -0400

    implemented images and styles on display images page

--- WOW_Assignment/Website/App_Start/BundleConfig.cs
+++ WOW_Assignment/Website/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace Website
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Styles/Product/css").Include(
+                            "~/Styles/Product.css"));
+
         }
     }
 }
