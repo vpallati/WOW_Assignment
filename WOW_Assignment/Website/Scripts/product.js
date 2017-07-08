f0379ec204caa89617e6fcd83ac6800476585360
Vishnu Vardhan Kumar Pallati <p.vishnuvardhankumar@gmail.com>
2017-07-08 12:38:41 -0400

    removed test projects

---
+++ WOW_Assignment/Website/Scripts/product.js
@@ -0,0 +1,10 @@
+﻿function preview(fileInput) {
+    //console.log("in Preview");
+    if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+        reader.onloadend = function (e) {
+            $('#ImagePreview').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
